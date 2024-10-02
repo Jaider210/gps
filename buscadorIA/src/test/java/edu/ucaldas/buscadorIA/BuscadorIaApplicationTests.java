56b65c1b04173aefe79224dff1c225dcf692f056
Jaider210 <125146104+Jaider210@users.noreply.github.com>
2024-10-01 20:18:06 -0500

    1

---
+++ buscadorIA/src/test/java/edu/ucaldas/buscadorIA/BuscadorIaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.buscadorIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
